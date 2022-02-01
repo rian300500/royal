25cea417b91cc2fc78879bb0c5dd622450dabec7
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2022-02-01 16:29:26 -0500

    Use PointBalance model's connection for advisory locks.

--- lib/royal/locking/advisory.rb
+++ lib/royal/locking/advisory.rb
@@ -10,7 +10,7 @@ module Royal
 
       # @param owner [ActiveRecord::Base]
       def call(owner)
-        owner.transaction(requires_new: true) do
+        PointBalance.transaction(requires_new: true) do
           acquire_advisory_lock_on_owner(owner)
           yield
         end
@@ -21,11 +21,11 @@ module Royal
       # @param owner [ActiveRecord::Base]
       # @return [void]
       def acquire_advisory_lock_on_owner(owner)
-        sql = owner.class.sanitize_sql_array([<<-SQL.squish, owner.class.polymorphic_name, owner.id & ID_LOBITS_MASK])
+        sql = PointBalance.sanitize_sql_array([<<-SQL.squish, owner.class.polymorphic_name, owner.id & ID_LOBITS_MASK])
           SELECT pg_advisory_xact_lock(hashtext(?), ?)
         SQL
 
-        owner.class.connection.query(sql)
+        PointBalance.connection.query(sql)
       end
     end
   end
